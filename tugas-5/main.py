be219078d63d4f2316b3945ea263684735cdd018
reyhannaufal <reyhannaufal21@gmail.com>
2022-05-31 12:32:51 +0700

    feat: implement getGoPackage with comment

--- tugas-5/main.py
+++ tugas-5/main.py
@@ -2,18 +2,16 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+# TODO: try catch mechanism, make file modular
 
 golang_doc = requests.get("https://go.dev/doc/")
 
 soup = BeautifulSoup(golang_doc.content, 'html.parser')
-# print(soup.prettify())
-# print(soup.title)
 html = list(soup.children)[2]
 body = list(html.children)[3]
 
 a_tag = BeautifulSoup(str(body), 'html.parser').find_all('a')
 
-# a tag with href "/doc/tutorial"
 a_doc = BeautifulSoup( str(a_tag), 'html.parser').find_all('a', href=re.compile('/doc/tutorial'))
 
 # print a doc
@@ -21,6 +19,19 @@ for idx, a in enumerate(a_doc):
     print(idx, ",akan mengambil: ", a.text)
 
 
-# print(a_doc)
-# print(list(body.children)[4])
+def getGoPackage(query, n):
+    golang_package = requests.get("https://pkg.go.dev/search?q=" + query)
+    golang_package = requests.get("https://pkg.go.dev/search?limit="+str(n)+"&m=package&q=" + query)
+    soup = BeautifulSoup(golang_package.content, 'html.parser')
+    packages_links = soup.find_all('div', class_=re.compile("SearchSnippet-headerContainer"))
+    packages_descriptions = soup.find_all('p', class_=re.compile("SearchSnippet-synopsis"))
 
+    print("\n")
+    for idx, package_link in enumerate(packages_links):
+        print(idx + 1, package_link.find('a').get('href'))
+        print(packages_descriptions[idx].text)
+        
+
+
+
+getGoPackage("llrb", 10)

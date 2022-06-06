5c02058b4e56fed59306d9f3bfe0b0355a9afcdf
Aristya14 <72466039+Aristya14@users.noreply.github.com>
2022-06-06 19:25:30 +0700

    no 3

--- tugas-3/pubsub_network.py
+++ tugas-3/pubsub_network.py
@@ -1,76 +1,86 @@
-import random, threading, time, zmq
-B = 32  # number of bits of precision in each random integer
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter08/queuepi.py
+# Small application that uses several different message queues
 
-def random_digits():
+import random, threading, time, zmq, sqlite3, os, sys, hashlib
+
+def randomizer():
     return str(random.randint(0, 2))
 
-def generator(zcontext, url):
-    zsock = zcontext.socket(zmq.PUB)
+def query(num):
+    db = sqlite3.connect("data.db")
+    cur = db.cursor()
+
+    sum = 0
+    value = 0
+    n1 = random.randint(1, 99000)
+    n2 = random.randint(1, 1000)
+    mod = "length(FirstNameLastName)"
+    sql = "select count(*) from MOCKDATA where (ID>={} AND ID<={}) AND {} % 3 = {};".format(n1, n1+n2, mod, num)
+    h = hash(sql)
+    if h % 2 == 0:
+        result = "n1 = {}, n2 = {}, num = {}, result = nol".format(n1, n2, num)
+    else:
+        cur.execute(sql)
+        value = int(cur.fetchone()[0])
+    
+    sum += value
+    result = "n1 = {}, n2 = {}, num = {}, result = {}".format(n1, n2, num, sum)
+    db.close()
+    return result
+
+def generator(zcontext, url, log_url):
+    """Produce random points in the unit square."""
+    zsock = zcontext.socket(zmq.REQ)
     zsock.bind(url)
-    while True:
-        zsock.send_string(random_digits())
-        print('Generated:', random_digits())
-        time.sleep(0.2)
-
-def executor_1(zcontext, in_url, out_url):
-    isock = zcontext.socket(zmq.SUB)
-    isock.connect(in_url)
-    isock.setsockopt(zmq.SUBSCRIBE, b'00')
     osock = zcontext.socket(zmq.PUSH)
-    osock.connect(out_url)
-    n1 = random.randint(1, 99000)
-    n2 = random.randint(1, 1000) 
-    data = str(n1) + ' ' + str(n2)
+    osock.connect(log_url)
     while True:
-        isock.recv_string()
-        osock.send_string(data)
+        zsock.send_string(randomizer())
+        result = zsock.recv_string()
+        osock.send_string(result)
 
-def executor_2(zcontext, in_url, out_url):
-    isock = zcontext.socket(zmq.SUB)
-    isock.connect(in_url)
-    isock.setsockopt(zmq.SUBSCRIBE, b'00')
-    osock = zcontext.socket(zmq.PUSH)
-    osock.connect(out_url)
-    n1 = random.randint(1, 99000)
-    n2 = random.randint(1, 1000) 
-    data = str(n1) + ' ' + str(n2)
+def executor(zcontext, url):
+    """Return the sum-of-squares of number sequences."""
+    zsock = zcontext.socket(zmq.REP)
+    for prefix in b'0', b'1':
+        zsock.setsockopt(zmq.IDENTITY, prefix)
+    zsock.connect(url)
     while True:
-        isock.recv_string()
-        osock.send_string(data)
+        result_generator = zsock.recv_string()
+        result = query(result_generator)
+        zsock.send_string(result)
 
-def executor_3(zcontext, in_url, out_url):
-    isock = zcontext.socket(zmq.SUB)
-    isock.connect(in_url)
-    isock.setsockopt(zmq.SUBSCRIBE, b'00')
-    osock = zcontext.socket(zmq.PUSH)
-    osock.connect(out_url)
-    n1 = random.randint(1, 99000)
-    n2 = random.randint(1, 1000) 
-    data = str(n1) + ' ' + str(n2)
+def executor_1(zcontext, url):
+    """Return the sum-of-squares of number sequences."""
+    zsock = zcontext.socket(zmq.REP)
+    zsock.setsockopt(zmq.HWM , b'1')
+    zsock.connect(url)
     while True:
-        isock.recv_string()
-        osock.send_string(data)
+        result_generator = zsock.recv_string()
+        result = query(result_generator)
+        zsock.send_string(result)
+        print("executor_1 berjalan")
 
-def logger(zcontext, url):
+def tally(zcontext, url):
+    """Tally how many points fall within the unit circle, and print pi."""
     zsock = zcontext.socket(zmq.PULL)
     zsock.bind(url)
-    while True:
-        log = zsock.recv_string()
-        print('Logger:', log)
+    result = zsock.recv_string()
+    print(result)
 
 def start_thread(function, *args):
     thread = threading.Thread(target=function, args=args)
-    thread.daemon = True  # so you can easily Ctrl-C the whole program
+    thread.daemon = True
     thread.start()
 
 def main(zcontext):
-    pubsub = 'tcp://127.0.0.1:6700'
+    reqrep = 'tcp://127.0.0.1:6701'
     pushpull = 'tcp://127.0.0.1:6702'
-    start_thread(generator, zcontext, pubsub)
-    start_thread(executor_1, zcontext, pubsub, pushpull)
-    # start_thread(executor_2, zcontext, pubsub, pushpull)
-    # start_thread(executor_3, zcontext, pubsub, pushpull)
-    start_thread(logger, zcontext, pushpull)
+    start_thread(generator, zcontext, reqrep, pushpull)
+    start_thread(executor, zcontext, reqrep)
+    start_thread(tally, zcontext, pushpull)
     time.sleep(5)
 
 if __name__ == '__main__':
